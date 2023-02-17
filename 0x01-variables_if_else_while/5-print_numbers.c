477f174135c457579505823ab1a458ba94183c8f
NourAzam <Noureldine.mfathy@gmail.com>
2023-02-17 01:07:39 -0800

    My first commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	int i=0;
-	while( i<=9){
-		printf("%d\n",i);
-		i++;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
 	}
-	return(0);
+	putchar('\n');
+	return (0);
 }
