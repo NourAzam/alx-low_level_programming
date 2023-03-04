5d7c6a3e584986a800c8829416c620ef70853cbc
NourAzam <Noureldine.mfathy@gmail.com>
2023-03-04 08:45:43 -0800

    C programming

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints numbers from 0 to 9.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints numbers
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
 		printf("%d", i);
+		i++;
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
